094b7cc6b3e7a8ad6350b2525f33ec8edfc681fe
mvdidier <49493246+mvdidier@users.noreply.github.com>
2020-01-25 10:56:42 -0600

    Modifiaciones a Variables y Algoritmos
    Modifiaciones a Variables y Algoritmos

--- Sesion3/src/Negocio/ConvertirBinarioAOctal.java
+++ Sesion3/src/Negocio/ConvertirBinarioAOctal.java
@@ -6,9 +6,22 @@ public class ConvertirBinarioAOctal implements ConversionEstrategias {
     public String Convertir(String ValorenBinario) {
 	 String Resultado=new String();
 	 try {
-		 
-		 
-		 
+		    long NumeroBinario = Long.parseLong(ValorenBinario);
+		    int NumeroOctal = 0, NumeroDecimal = 0, i = 0;
+	        while(NumeroBinario != 0)
+	        {
+	        	NumeroDecimal += (NumeroBinario % 10) * Math.pow(2, i);
+	            ++i;
+	            NumeroBinario /= 10;
+	        }
+	        i = 1;
+	        while (NumeroDecimal != 0)
+	        {
+	        	NumeroOctal += (NumeroDecimal % 8) * i;
+	        	NumeroDecimal /= 8;
+	            i *= 10;
+	        }
+		    Resultado = Long.toString(NumeroOctal);		
 		}
 		catch(Exception e) {
 			Resultado = "Ocurrió un error en la conversión: " + e.getMessage();
--- Sesion3/src/Negocio/ConvertirOctalABinario.java
+++ Sesion3/src/Negocio/ConvertirOctalABinario.java
@@ -6,7 +6,18 @@ public class ConvertirOctalABinario implements ConversionEstrategias {
     public String Convertir(String ValorenOctal) {
 	 String Resultado=new String();
 	 try {
-		 Resultado = "Pendiente por implementar";
+		    int binnum, rem, quot, i=1, j;
+		    int octnum[] = new int[100];
+		    quot = Integer.parseInt(ValorenOctal);
+		    while(quot != 0)
+	        {
+	            octnum[i++] = quot%8;
+	            quot = quot/8;
+	        }
+	        for(j=i-1; j>0; j--)
+	        {
+	            Resultado += octnum[j];
+	        }
 		}
 		catch(Exception e) {
 			Resultado = "Ocurrió un error en la conversión: " + e.getMessage();
